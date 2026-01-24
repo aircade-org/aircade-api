41d4c00a25e1ea1d781e447a95169e972093f325
Nicolas Spijkerman <nicospijke@gmail.com>
2026-01-24 15:54:05 +0100

    Setup linting configuration, build profiles, and add code dependencies

---
+++ src/api.rs
@@ -0,0 +1,9 @@
+use crate::Result;
+
+pub struct Api {}
+
+impl Api {
+    pub fn launch() -> Result<()> {
+        Ok(())
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,1 +1,17 @@
-fn main() {}
+mod api;
+mod config;
+mod dto;
+mod entities;
+mod errors;
+mod middleware;
+mod migrations;
+mod routes;
+mod services;
+mod utils;
+
+use anyhow::Result;
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    api::Api::launch()
+}
